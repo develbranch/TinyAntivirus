ebbaeb5ff379799d07fb9fd0ee760857e4988185
quangnh89 <quangnh89@gmail.com>
2016-07-21 23:57:15 +0700

    build with x86 only

--- ci/windows/test_appveyor.bat
+++ ci/windows/test_appveyor.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
-set APP_PATH="%2"\Unittests.exe
-if "%1" == "x64" (set APP_PATH=x64\"%2"\Unittests.exe)
+set APP_PATH="%2\Unittests.exe"
+if "%1" == "x64" (set APP_PATH="x64\%2\Unittests.exe")
+echo  "%1"
+echo  "%2"
+echo  %APP_PATH%
 %APP_PATH% "%SAMPLE_DIR%"
 
